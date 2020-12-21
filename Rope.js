86d4671d28db613f7d5d0515640c9a5057997a48
Saanvi Gupta <65902729+Saanvi2828@users.noreply.github.com>
2020-12-21 11:57:13 +0530

    Add files via upload

--- Rope.js
+++ Rope.js
@@ -1,18 +1,18 @@
 class Rope{
-    constructor(body1,body2,offsetX,offsetY){
-        this.offsetX=offsetX,
-        this.offsetY=offsetY
-      var options={
+    constructor(body1,pointB){
+       var options={
+          isStatic:false,
           bodyA:body1,
-          bodyB:body2,
-          pointB:{x:this.offsetX,y:this.offsetY}
+          length: 400,
+          pointB:pointB
       }
+      this.pointB=pointB
       this.rope=Matter.Constraint.create(options)
       World.add(world,this.rope);
     }
     display(){
         strokeWeight(4);
         stroke("black");
-        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+        line(this.rope.bodyA.position.x,this.rope.bodyA.position.y,this.pointB.x,this.pointB.y);
     }
 }
--- sketch.js
+++ sketch.js
@@ -20,9 +20,17 @@ function setup() {
 
 	//Create the Bodies Here.
 	roof=new Roof(400,25,750,50);
-	ball1=new Ball(200,700,20);
+	ball1=new Ball(width/2-80,700,20);
+	ball2=new Ball(width/2-40,700,20);
+	ball3=new Ball(width/2,700,20);
+	ball4=new Ball(width/2+40,700,20);
+	ball5=new Ball(width/2+80,700,20);
 
-	rope=new Rope(ball1.body,roof.body,-40,0);
+	rope=new Rope(ball1.body,{x:width/2-80,y:50});
+	rope1=new Rope(ball2.body,{x:width/2-40,y:50});
+	rope2=new Rope(ball3.body,{x:width/2,y:50});
+	rope3=new Rope(ball4.body,{x:width/2+40,y:50});
+	rope4=new Rope(ball5.body,{x:width/2+80,y:50});
 	//Engine.run(engine);
   
 }
@@ -34,11 +42,24 @@ function draw() {
 Engine.update(engine);
   roof.display();
   ball1.display();
-  
+  ball2.display();
+  ball3.display();
+  ball4.display();
+  ball5.display();
+  rope.display();
+  rope1.display();
+  rope2.display();
+  rope4.display();
+  rope3.display();
   rectMode(CENTER);
   
   drawSprites();
- 
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball1.body,ball1.body.position,{x:-50,y:-45})
+	  }
 }
 
 
